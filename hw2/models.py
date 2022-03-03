1f96af3ab73332d752433b00e6524e5b9133f59a
tkabir1 <83760134+tkabir1@users.noreply.github.com>
2022-03-03 16:16:33 -0500

    Update models.py

--- hw2/models.py
+++ hw2/models.py
@@ -5,10 +5,17 @@ from qbdata import WikiLookup
 import torch
 from transformers import BertForSequenceClassification, pipeline
 from transformers import AutoTokenizer, AutoModelForQuestionAnswering, AutoModelForSequenceClassification
-
+from transformers import TrainingArguments, Trainer
+# ADDED
+from datasets import load_metric
+import numpy as np
+from transformers import default_data_collator, get_cosine_with_hard_restarts_schedule_with_warmup, AdamW,DataCollatorWithPadding
+from transformers import EarlyStoppingCallback
 # Change this based on the GPU you use on your machine
 device = 'cuda:0' if torch.cuda.is_available() else 'cpu'
-
+print( torch.cuda.is_available() )
+devices = [d for d in range(torch.cuda.device_count())]
+print( [torch.cuda.get_device_name(d) for d in devices] )
 
 class Guesser(BaseGuesser):
     """You can implement your own Bert based Guesser here"""
@@ -18,26 +25,18 @@ class Guesser(BaseGuesser):
 class ReRanker(BaseReRanker):
     """A Bert based Reranker that consumes a reference passage and a question as input text and predicts the similarity score: 
         likelihood for the passage to contain the answer to the question.
-
     Task: Load any pretrained BERT-based and finetune on QuizBowl (or external) examples to enable this model to predict scores 
         for each reference text for an input question, and use that score to rerank the reference texts.
-
     Hint: Try to create good negative samples for this binary classification / score regression task.
-
     Documentation Links:
-
         Pretrained Tokenizers:
             https://huggingface.co/docs/transformers/v4.16.2/en/main_classes/model#transformers.PreTrainedModel.from_pretrained
-
         BERT for Sequence Classification:
             https://huggingface.co/docs/transformers/v4.16.2/en/model_doc/bert#transformers.BertForSequenceClassification
-
         SequenceClassifierOutput:
             https://huggingface.co/docs/transformers/v4.16.2/en/main_classes/output#transformers.modeling_outputs.SequenceClassifierOutput
-
         Fine Tuning BERT for Seq Classification:
             https://huggingface.co/docs/transformers/master/en/custom_datasets#sequence-classification-with-imdb-reviews
-
         Passage Reranking:
             https://huggingface.co/amberoad/bert-multilingual-passage-reranking-msmarco
     """
@@ -51,9 +50,38 @@ class ReRanker(BaseReRanker):
         self.model = AutoModelForSequenceClassification.from_pretrained(
             model_identifier, num_labels=2).to(device)
 
-    def train(self):
+    def train(self, training_dataset, wiki_lookup):
         """Fill this method with code that finetunes Sequence Classification task on QuizBowl questions and passages.
         Feel free to change and modify the signature of the method to suit your needs."""
+        # create hugging face trainer
+        #print(training_dataset[0])
+        def preprocess_function(examples):
+            print()
+            text=self.tokenizer(examples["text"], truncation=True)
+            answer=self.tokenizer(examples["answer"], truncation=True)
+            return self.tokenizer(examples["text"], truncation=True)
+        tokenized_dataset = training_dataset.map(preprocess_function, batched=True)
+        data_collator = DataCollatorWithPadding(tokenizer=self.tokenizer)
+        training_args = TrainingArguments(
+            output_dir="./results",
+            #learning_rate=2e^-5,
+            per_device_train_batch_size=6,
+            per_device_eval_batch_size=6,
+            num_train_epochs=5,
+            weight_decay=0.01,
+        )
+
+        trainer = Trainer(
+            model=self.model,
+            args=training_args,
+            train_dataset=tokenized_dataset["train"],
+            eval_dataset=tokenized_dataset["eval"],
+            tokenizer=self.tokenizer,
+            data_collator=data_collator,
+        )
+
+        trainer.train()
+
         pass
 
     def get_best_document(self, question: str, ref_texts: List[str]) -> int:
@@ -109,18 +137,13 @@ class Retriever:
 
 class AnswerExtractor:
     """Load a huggingface model of type transformers.AutoModelForQuestionAnswering and finetune it for QuizBowl questions.
-
     Documentation Links:
-
         Extractive QA: 
             https://huggingface.co/docs/transformers/v4.16.2/en/task_summary#extractive-question-answering
-
         QA Pipeline: 
             https://huggingface.co/docs/transformers/v4.16.2/en/main_classes/pipelines#transformers.QuestionAnsweringPipeline
-
         QAModelOutput: 
             https://huggingface.co/docs/transformers/v4.16.2/en/main_classes/output#transformers.modeling_outputs.QuestionAnsweringModelOutput
-
         Finetuning Answer Extraction:
             https://huggingface.co/docs/transformers/master/en/custom_datasets#question-answering-with-squad
     """
@@ -138,9 +161,110 @@ class AnswerExtractor:
         self.model = AutoModelForQuestionAnswering.from_pretrained(
             model_identifier).to(device)
 
-    def train(self):
+    def train(self, training_dataset, wiki_lookup):
         """Fill this method with code that finetunes Answer Extraction task on QuizBowl examples.
         Feel free to change and modify the signature of the method to suit your needs."""
+
+        # modified from Huggingface QA fine tuning tutorial
+        def preprocess_function(examples):
+            questions = [q.strip() for q in examples["text"]]
+            first_sentences = [q.strip() for q in examples["first_sentence"]]
+            # print( wiki_lookup[ examples["page"][0]])
+            wiki_texts = list(map( lambda x: wiki_lookup[ x ][ "text" ], examples["page"]))
+            # print(len([*questions,*map(lambda x: x.split(".")[0], questions )]))
+            # print(len([ *wiki_texts, *wiki_texts]))
+            inputs = self.tokenizer(
+                [*questions,*first_sentences],
+                [ *wiki_texts, *wiki_texts ],
+                truncation=True,
+                return_offsets_mapping=True,
+                padding="max_length",
+            )
+
+            offset_mapping = inputs.pop("offset_mapping")
+            answers = [ *examples["answer"], *examples["answer"] ]
+            # print(len(answers))
+            start_positions = []
+            end_positions = []
+
+            for i, offset in enumerate(offset_mapping):
+                answer = answers[i]
+                start_char = 0
+                end_char = len(answer)
+                sequence_ids = inputs.sequence_ids(i)
+
+                # Find the start and end of the context
+                idx = 0
+                while sequence_ids[idx] != 1:
+                    idx += 1
+                context_start = idx
+                while sequence_ids[idx] == 1:
+                    idx += 1
+                context_end = idx - 1
+
+                # If the answer is not fully inside the context, label it (0, 0)
+                if offset[context_start][0] > end_char or offset[context_end][1] < start_char:
+                    start_positions.append(0)
+                    end_positions.append(0)
+                else:
+                    # Otherwise it's the start and end token positions
+                    idx = context_start
+                    while idx <= context_end and offset[idx][0] <= start_char:
+                        idx += 1
+                    start_positions.append(idx - 1)
+
+                    idx = context_end
+                    while idx >= context_start and offset[idx][1] >= end_char:
+                        idx -= 1
+                    end_positions.append(idx + 1)
+
+            inputs["start_positions"] = start_positions
+            inputs["end_positions"] = end_positions
+            return inputs
+
+        tokenized_dataset = training_dataset.map(preprocess_function, batched=True,
+                                                 remove_columns=training_dataset["train"].column_names)
+        tokenized_dataset.shuffle(seed=0)
+        data_collator = default_data_collator
+        # metric = load_metric("accuracy")
+        training_args = TrainingArguments(
+            output_dir="./results",
+            evaluation_strategy="epoch",
+            save_strategy="epoch",
+            # learning_rate=3e-5,
+            per_device_train_batch_size=4,
+            per_device_eval_batch_size=4,
+            num_train_epochs=20,
+            # weight_decay=0.01,
+            # gradient_checkpointing=True,
+            fp16=True,
+            load_best_model_at_end=True,
+            metric_for_best_model="eval_loss",
+            save_total_limit=6,
+            dataloader_num_workers=2,
+            logging_steps=72
+            # eval_accumulation_steps=8
+        )
+        num_training_steps = 20 * len( tokenized_dataset["train"] )
+        optimizer = AdamW(self.model.parameters(),lr=2e-5, weight_decay=0.01)
+        lr_scheduler = get_cosine_with_hard_restarts_schedule_with_warmup(
+                optimizer=optimizer,
+                num_warmup_steps=1//7*num_training_steps//5,
+                num_training_steps=num_training_steps//5,
+                num_cycles=5,
+            )
+        trainer = Trainer(
+            model=self.model,
+            args=training_args,
+            train_dataset=tokenized_dataset["train"],
+            eval_dataset=tokenized_dataset["eval"],
+            data_collator=data_collator,
+            tokenizer=self.tokenizer,
+            callbacks=[EarlyStoppingCallback(early_stopping_patience=4)],
+            optimizers=(optimizer,lr_scheduler)
+            # compute_metrics=metric
+        )
+        trainer.train()
         pass
 
     def extract_answer(self, question: Union[str,List[str]], ref_text: Union[str, List[str]]) -> List[str]:
@@ -159,4 +283,3 @@ class AnswerExtractor:
             answer_ids = [tokens[s:e] for tokens, s, e in zip(input_tokens, start_index, end_index)]
 
             return self.tokenizer.batch_decode(answer_ids)
-        
