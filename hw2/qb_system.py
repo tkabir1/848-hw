d794b2d2a138e8c799595290dfb736b3a3ce9f11
tkabir1 <83760134+tkabir1@users.noreply.github.com>
2022-03-03 16:17:28 -0500

    Update qb_system.py

--- hw2/qb_system.py
+++ hw2/qb_system.py
@@ -5,13 +5,14 @@ import torch
 from pip import main
 from tfidf_guesser import TfidfGuesser
 from models import AnswerExtractor, Retriever, ReRanker, WikiLookup
+from datasets import load_dataset
 
 
 class QuizBowlSystem:
 
-    def __init__(self, wiki_lookup_path: str = '../data/wiki_lookup.2018.json') -> None:
+    def __init__(self, training_set="../data/small.guesstrain.json", eval_set="../data/small.guessdev.json") -> None:
         """Fill this method to create attributes, load saved models, etc
-        Don't add any other arguments to this constructor. 
+        Don't have any arguments to this constructor. 
         If you really want to have arguments, they should have some default values set.
         """
         guesser = TfidfGuesser()
@@ -19,26 +20,34 @@ class QuizBowlSystem:
         guesser.load('models/tfidf.pickle')
 
         print('Loding the Wiki Lookups...')
-        self.wiki_lookup = WikiLookup(wiki_lookup_path)
+        self.wiki_lookup = WikiLookup('../data/wiki_lookup.2018.json')
 
-        reranker = ReRanker()
+        self.reranker = ReRanker()
         print('Loading the Reranker model...')
-        reranker.load('amberoad/bert-multilingual-passage-reranking-msmarco')
+        self.reranker.load('amberoad/bert-multilingual-passage-reranking-msmarco')
+        training_dataset = load_dataset("json",data_files={"train":training_set, "eval":eval_set }
+                                        ,field="questions")
+        print("Training the Reranker model...")
+        self.reranker.train(training_dataset,self.wiki_lookup)
 
-        self.retriever = Retriever(guesser, reranker, wiki_lookup=self.wiki_lookup)
-
+        self.retriever = Retriever(guesser, self.reranker, wiki_lookup=self.wiki_lookup)
+        
         answer_extractor_base_model = "csarron/bert-base-uncased-squad-v1"
         self.answer_extractor = AnswerExtractor()
         print('Loading the Answer Extractor model...')
         self.answer_extractor.load(answer_extractor_base_model)
-
+        print( "Constructing Datasets...")
+        training_dataset = load_dataset("json",data_files={"train":training_set, "eval":eval_set }
+                                        ,field="questions")
+        print("Training the Answer Extractor model...")
+        self.answer_extractor.train(training_dataset,self.wiki_lookup)
+        
     def retrieve_page(self, question: str, disable_reranking=False) -> str:
         """Retrieves the wikipedia page name for an input question."""
         with torch.no_grad():
-            page = self.retriever.retrieve_answer_document(
-                question, disable_reranking=disable_reranking)
+            page = self.retriever.retrieve_answer_document(question, disable_reranking=disable_reranking)
             return page
-
+    
     def execute_query(self, question: str, *, get_page=True) -> str:
         """Populate this method to do the following:
         1. Use the Retriever to get the top wikipedia page.
@@ -47,9 +56,8 @@ class QuizBowlSystem:
         with torch.no_grad():
             page = self.retrieve_page(question, disable_reranking=True)
             reference_text = self.wiki_lookup[page]['text']
-            answer = self.answer_extractor.extract_answer(
-                question, reference_text)[0]  # singleton list
-            return (answer, page) if get_page else answer
+            answer = self.answer_extractor.extract_answer(question, reference_text)[0] # singleton list
+            return answer, page if get_page else answer
 
 
 if __name__ == "__main__":
